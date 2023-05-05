120d7c82c7e588e88f712aa48f2444ae3ef6f9f6
Nanre1027 <amorimrenan597@gmail.com>
2023-05-05 09:23:46 -0300

    Declarar ar/let/const dif- hoistin op/Mat interpol

---
+++ js/script.js
@@ -0,0 +1,60 @@
+console.log("Olá Mundo!");
+ //declarar uma variável
+ ///    var nr1 = "10";
+ ///    var nr2 = "5";
+
+ //imprimir as variáveis com o objeo console
+ ///    console.log(nr1);
+ ///    console.log(nr2);
+
+ //Realizando as operações básicas com esses valores:
+ //+ _ - _ * _ e /
+
+ //adição
+ //realize a concatenação com o texto resultado:
+ //Ex: console.log(Resultado nr1 + nr2);
+ ///    console.log(nr1 + nr2);
+ ///    console.log("Resultado: " + (parseInt (nr1) + parseInt(nr2)));
+ ///    console.log(`RESULTADO ADIÇÃO : (${parseInt (nr1) + parseInt (nr2)})`);
+
+
+ //Subtração
+ ///    console.log(`RESULTADO SUBTRAÇÃO : (${parseInt (nr1) - parseInt (nr2)})`);
+
+
+ //Multiplicação
+ ///    console.log(`RESULTADO MULTIPLICAÇÃO : (${parseInt (nr1) * parseInt (nr2)})`);
+
+
+ //Divisão
+ ///    console.log(`RESULTADO DIVISÃO : (${parseInt (nr1) / parseInt (nr2)})`);
+
+
+ //Resto da ivisão
+ ///    console.log(`RESTO DA DIVISÃO : (${parseInt (nr1) % parseInt (nr2)})`);
+
+ //Descobrindo se um número é par ou impar
+ //Utilizando operador ternário ( ? ).
+ ///    var nr1 = "10";
+ ///    var nr2 = "5";
+ ///    var resultado;
+
+ ///    resultado = !(parseInt(nr1)%parseInt(nr2)) ? "PAR" : "IMPAR";
+ ///    console.log(`O Número é : ${resultado}`);
+
+    //DIFERENÇA ENTRE VAR/LET/CONST
+    let nome1 = "Beatriz";
+
+    if(nome != ""){
+       let nome = "Robson";
+    }
+    //Qual o nome será impresso
+    console.log(nome);
+
+    let nom2 = "Matheus";
+
+    //REGRAS DO CONST
+    //1ª Regra = Incializar na declaração
+    const nome3 = "Violante";
+    //2ª Regra = Não pode ser alterado durante o fluxo do programa.
+    const nome4 = "Joathan";
