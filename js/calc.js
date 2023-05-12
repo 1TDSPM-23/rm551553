8dfa9bb15472bfd96b846046c0ab00891f15f173
Nanre1027 <amorimrenan597@gmail.com>
2023-05-12 09:58:33 -0300

    adicionando mais teclas para a calculadora.

--- js/calc.js
+++ js/calc.js
@@ -1,9 +1,35 @@
 
 function insereNumeroNoVisor(botao,visor) {
-    visor.value += botao.value;
-}
 
+    if(botao.value == "+" || botao.value == "-" || botao.value == "*" || botao.value == "/"){
+
+        //verificando o penultimo caractere do visor utilizando o atribtp lenght
+        if(visor.value[visor.value.length-1] == "+" || visor.value[visor.value.length-1] == "-" || visor.value[visor.value.length-1] == "*" || visor.value[visor.value.length-1] == "/" ){
+            visor.value.slice(1);
+        }else{
+            visor.value += botao.value;
+        }
+
+    }else{
+        visor.value += botao.value;
+    }
+
+
+
+
+}
 const resultadoDasOperacoes = (visor) =>{
-    //Utilizando a função eva(expressão)
-    visor. value = eval(visor.value);
+
+    try {
+        //Utilizando a função eval(expressão)
+        visor.value = eval(visor.value);
+    } catch (error) {
+        if(visor.value[visor.value.length] == "+" || visor.value[visor.value.length] == "-" || visor.value[visor.value.length] == "*" || visor.value[visor.value.length] == "/" ){
+            visor.value = visor.value.slice(visor.value.length-1);
+        }else{
+            visor.value.shift(1);
+        } 
+    }
+
+
 }
